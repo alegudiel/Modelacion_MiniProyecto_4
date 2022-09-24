8f5b5bbe3128dd8d01c385e4e3f28f994e1dbfed
Ale Gudiel <gud19232@uvg.edu.gt>
2022-09-24 14:57:31 -0600

    Update Ej1.py

--- Ej1.py
+++ Ej1.py
@@ -13,7 +13,6 @@ Nd = 0 # Number of departures
 N = 0 # Number of requests on time T
 Ta = 0 # Time of arrival
 Td = 0 # Time of departure
-noServers = 1 # Number of servers
 
 # Times of arrival and departure on i
 A = []
@@ -30,9 +29,7 @@ t = 0
 while t < T:
     t += funcExp(t, 40)
 
-    if (N == 1):
-        print("valor ta", Ta, Td, N, t)
-
+    # Caso 1
     # Request arrived, Time is running
     if (Ta <= Td and Ta <= T):
         t = Ta
@@ -41,24 +38,25 @@ while t < T:
         t_t = funcExp(Ta, 40) # Ta - (1/100) * math.log(random.random())
         Ta = t_t
         if (N == 1):
-            # print("Arrived")
-            Td = -(1/100) * math.log(random.random())
+            Td = -(1/100) * math.log(random.random()) ##sería para el primer proveedor
+            # Td = -(1/10) * math.log(random.random()) ##sería para el segundo proveedor
         
-            A.append(Ta)
+        A.append(Ta)
         
+    # Caso 2
     if (Td < Ta and Td <= T):
         # Request finished, Time is running
         t = Td
         N -= 1
         Nd += 1
-        if (N == 0):
-            # print("We don't know")
-            Td = inf
-        else:
+        if (N != 0):
+        #     Td = inf
+        # else:
             Td = t + funcExp(Td, 40)
 
         D.append(t)
 
+    # Caso 3
     if (min(Ta, Td) > T and N > 0):
         # Request arrived but has to wait
         t = Td
@@ -69,14 +67,17 @@ while t < T:
             Td = t + funcExp(Td, 40)
         D.append(t)
 
-        
+    # Caso 4
     if (min(Ta, Td) > T , N == 0):
         # Request arrived after time, no request ton queue
         Tp = max(t - T, 0)
         # break
 
 
-# print("Llegaron " , len(A), Tp, Td, Ta)
-# print("Salieron " , len(D))
-print(D[:10])
-print(A[:10])
+print("A) Solicitudes atendidas por el servidor: ", Nd)
+print("B) Tiempo que el servidor estuvo ocupado: ", Ta+Td)
+print("C) Tiempo que el servidor estuvo desocupado: ", -T+Td)
+print("D) Tiempo total estuvieron las solicitudes en el sistema: ", Td + Tp)
+print("E) Tiempo promedio de espera en la cola: ", Td/Nd)
+print("F) En promedio ", Na/T, " solicitudes estuvieron en cola cada segundo")
+print("G) Momento de salida de la última solicitud: ", D[-1])
